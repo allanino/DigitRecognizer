a94f207f4b9e18aa3dcc3fe14f73de656334a37e
Allan Costa <allaninocencio@yahoo.com.br>
2013-09-10 10:43:19 -0300

    Update visual.py

--- visual.py
+++ visual.py
@@ -4,15 +4,15 @@ import csv
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-	# only to generate submission.npy
-f="knnPredictions.csv"
+	# only to generate predictions.npy
+f="knnPredictions.csv" # file with predictions
 csv_reader = csv.reader(open(f, "r"))
 data = []
 for row in csv_reader:
 	data.append(np.array(np.int64(row)))
 np.save('predictions.npy', data)
 
-	# to plot the train sample
+	# to plot the train sample 
 # image = np.load('train_small.npy')
 # prediction = np.load('labels_small.npy')
 
@@ -20,7 +20,7 @@ np.save('predictions.npy', data)
 image = np.load('test_small.npy')
 prediction = np.load('predictions.npy')
 
-img = np.eye(28,dtype=int)
+img = np.empty((28, 28),dtype=int)
 
 # Show images and predictions from lines argv[1] to argv[2]
 for k in range(int(argv[1]),int(argv[2])):
