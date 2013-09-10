940add6d5ec4a3b704bd93a702e95bb16774a7d8
Plano Be <planobe@Plano-Bes-iMac.local>
2013-09-10 11:38:50 -0300

    Modified parameters

--- KNN.py
+++ KNN.py
@@ -27,7 +27,7 @@ def read_data(f, header=True, test=False, rows=0):
 
 def predictKNN(train,labels,test):
     print 'start knn'
-    knn = neighbors.KNeighborsClassifier()
+    knn = neighbors.KNeighborsClassifier(weights='distance')
     knn.fit(train, labels) 
     probabilities = knn.predict_proba(test)
     predictions = knn.predict(test)
@@ -45,28 +45,23 @@ class PredScore:
 if __name__ == '__main__':
     print 'read data!'
     #only use the below for initial creation of npy files
-    #train, labels = read_data("train.csv", rows=1000)
-    #np.save('train_small.npy', train)
-    #np.save('labels_small.npy', labels)
+    train, labels = read_data("train.csv", rows=1000)
+    np.save('train_small.npy', train)
+    np.save('labels_small.npy', labels)
 
     train = np.load('train_small.npy')
     labels = np.load('labels_small.npy')
-
-    #train = np.load('train.npy')
-    #labels = np.load('labels.npy')
-#    
+ 
     print 'done reading train'
     
     #only use the below for the initial creation of npy files.
-    #test, tmpl = read_data("test.csv", test=True, rows=1000)
-    #np.save('test_small.npy', test)
-    #np.save('tmpl_small.npy', tmpl)
+    test, tmpl = read_data("test.csv", test=True, rows=1000)
+    np.save('test_small.npy', test)
+    np.save('tmpl_small.npy', tmpl)
 
     test = np.load('test_small.npy')
     tmpl = np.load('tmpl_small.npy')
 
-    #test = np.load('test.npy')
-    #tmpl = np.load('tmpl.npy')
     print 'done reading test!'
 
     knnPredictions, knnScore = predictKNN(train,labels, test)
